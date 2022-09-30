81640483b7e82fce2b7d697fff32512a150d4a16
Надюшка Рыбка <kitti-46@ya.ru>
2022-09-30 22:29:04 +0300

    added ex 007

---
+++ Ex07/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int xa = 40;
+int ya = 1;
+int xb = 1;
+int yb = 30;
+int xc = 80;
+int yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
